7d926a635fa8d3b09b1bdb6e725a534631093349
kate smith <kateesmith4994@gmail.com>
2020-11-11 10:39:37 +0000

    failing test for fizzbuzz

--- Fizzbuzz_test.go
+++ Fizzbuzz_test.go
@@ -5,28 +5,29 @@ import "testing"
 func TestSum(t *testing.T) {
 
 	t.Run("Test for Fizz", func(t *testing.T) {
-
 		got := Sum(3)
 		want := "Fizz"
-
 		assertEquals(t, got, want)
 	})
 
 	t.Run("Test for Buzz", func(t *testing.T) {
-
 		got := Sum(5)
 		want := "Buzz"
-
 		assertEquals(t, got, want)
 	})
 
 	t.Run("Test for normal integer", func(t *testing.T) {
-
 		got := Sum(4)
 		want := "4"
-
 		assertEquals(t, got, want)
 	})
+
+	t.Run("Test for Fizzbuzz", func(t *testing.T) {
+		got := Sum(15)
+		want := "Fizzbuzz"
+		assertEquals(t, got, want)
+	})
+
 }
 
 func assertEquals(t *testing.T, got string, want string) {
